90668bb4c11137e81582fe1fe36b2a93ba70e974
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-05-03 03:13:18 +0200

    fix grid

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -25,7 +25,7 @@ function LoadMore() {
 
   return (
     <>
-      <section className='grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-10'>
+      <section className='grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
         {data}
       </section>
       <section className='flex justify-center items-center w-full'>
