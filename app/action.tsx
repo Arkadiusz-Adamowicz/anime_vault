80d50334782469cfd21562a17f4c592fb872e3f1
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-06-04 16:16:54 +0200

    update

--- app/action.tsx
+++ app/action.tsx
@@ -3,9 +3,11 @@
 import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
 
 export const fetchAnime = async (page: number) => {
-  const res = await fetch(
-    `https://shikimori.one/api/animes?page=${page}&limit=8&order=name&censored=false&genre=9`
-  );
+  const ecchi = `https://shikimori.one/api/animes?page=${page}&limit=8&order=name&censored=false&genre=9`;
+  const hentai = `https://shikimori.one/api/animes?page=${page}&limit=8&order=name&censored=false&genre=12`;
+  const latest = `https://shikimori.one/api/animes?page=${page}&status=latest&limit=8&order=name`;
+
+  const res = await fetch(ecchi);
   const data = await res.json();
 
   return data.map((item: AnimeProp, index: number) => (
