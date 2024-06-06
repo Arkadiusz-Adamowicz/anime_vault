4ff965bc7f552d20b4ed08e61128381eda2c7183
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-06-06 15:52:01 +0200

    update

--- app/action.tsx
+++ app/action.tsx
@@ -3,13 +3,8 @@
 import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
 
 export const fetchAnime = async (page: number) => {
-<<<<<<< HEAD
   const ecchi = `https://shikimori.one/api/animes?page=${page}&order=name&censored=false&genre=9`;
   const hentai = `https://shikimori.one/api/animes?page=${page}&order=name&censored=false&genre=12`;
-=======
-  const ecchi = `https://shikimori.one/api/animes?page=${page}&limit=12&order=name&censored=false&genre=9`;
-  const hentai = `https://shikimori.one/api/animes?page=${page}&limit=12&order=name&censored=false&genre=12`;
->>>>>>> d7de0972d4adc4b8706596a18c76bcb1bfa91a01
 
   const res = await fetch(ecchi);
   const data = await res.json();
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,8 @@ const Anime = async () => {
   return (
     <div className='sm:p-16 py-16 px-8 flex flex-col gap-10'>
       <h2 className='text-3xl text-white font-bold'>Explore Anime</h2>
-<<<<<<< HEAD
       <section>
         <FetchAnime />
-=======
-      <section className='grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
-        {data}
->>>>>>> d7de0972d4adc4b8706596a18c76bcb1bfa91a01
       </section>
     </div>
   );
